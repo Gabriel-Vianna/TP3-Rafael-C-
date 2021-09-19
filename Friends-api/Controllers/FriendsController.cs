06f143f152e5e8c20bcfa412495b0d9ec8f0f64a
Gabriel Vianna <gabriel1_vianna@hotmail.com>
2021-09-19 20:48:56 -0300

    first blood

---
+++ Friends-api/Controllers/FriendsController.cs
@@ -0,0 +1,94 @@
+﻿using Friends_api.Data;
+using Friends_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Friends_api.Controllers
+{
+    [ApiController]
+    [Route("api/friends")]
+    public class FriendsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult getAll()
+        {
+            var repository = new Repository();
+
+            return new OkObjectResult(repository.GetAll());
+        }
+
+        [HttpGet]
+        [Route("id")]
+        public ActionResult GetById(int id)
+        {
+            var repository = new Repository();
+
+            var friend = repository.GetById(id);
+
+            if (friend == null)
+                return new NotFoundObjectResult(new { message = "Não existe amigo com este Id" });
+
+            return new OkObjectResult(friend);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult Create([FromBody] Friends req)
+        {
+            var repository = new Repository();
+
+            Friends friend = req;
+
+            if (friend == null)
+                return new BadRequestObjectResult(new { message = "Dados para criação de um amigo é necessário" });
+
+            repository.Save(friend);
+
+            return new OkObjectResult(req);
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public ActionResult Delete(int id)
+        {
+            var repository = new Repository();
+
+            var friend = repository.GetById(id);
+
+            if (friend == null)
+                return new NotFoundObjectResult(new { message = "Não existe amigo com este Id" });
+
+            repository.Delete(friend.Id);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("")]
+        public ActionResult Update([FromBody] Friends req)
+        {
+            var repository = new Repository();
+
+            Friends friend = req;
+
+            Friends friendInDataBase = repository.GetById(friend.Id);
+
+            if(friendInDataBase == null)
+                return new BadRequestObjectResult(new { message = "Não existe amigo registrado com este Id" });
+
+            if (friend == null)
+                return new BadRequestObjectResult(new { message = "Necessários dados para a edição de um amigo" });
+
+            repository.Update(friend);
+
+            return new OkObjectResult(req);
+        }
+    }
+}
---
+++ Friends-api/Data/Repository.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using Friends_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friends_api.Data
+{
+    public class Repository
+    {
+        private string ConnectionString = "Server=tcp:infnet.database.windows.net,1433;Initial Catalog=friends-database;Persist Security Info=False;User ID=gvianna;Password=Ervilhag140994;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
+
+        public List<Friends> GetAll()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var sql = "SELECT * FROM Friends";
+                return connection.Query<Friends>(sql).ToList();
+            }
+        }
+
+        public Friends GetById(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var sql = "SELECT * FROM Friends where id = @id";
+                return connection.QueryFirstOrDefault<Friends>(sql, new { id = id });
+            }
+
+        }
+
+        public int Save(Friends friend)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var sql = @"    INSERT INTO Friends (FirstName, LastName, Email, Phone, BirthDate)
+                                                   Values (@FirstName, @LastName, @Email, @Phone, @BirthDate);
+                                       SELECT @@IDENTITY;
+                            ";
+
+                var lastId = connection.ExecuteScalar<int>(sql, friend);
+                return lastId;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var sql = @"DELETE FROM Friends where id = @id";
+
+                connection.Execute(sql, new { id = id});
+            }
+        }
+
+        public int Update(Friends friend)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var sql = @"UPDATE Friends SET FirstName = @FirstName, LastName = @LastName, Email = @Email, Phone = @Phone, BirthDate = @BirthDate
+                            WHERE id = @id";
+
+                var friendUpdated = connection.Execute(sql, friend);
+                return friendUpdated;
+            }
+        }
+    }
+}
